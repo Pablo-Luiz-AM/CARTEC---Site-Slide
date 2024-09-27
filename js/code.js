c4ebcd30abe4beeef93cbb3c37ae2623d3d6a133
pablo.profissional.de.ti@gmail.com <pablo.profissional.de.ti@gmail.com>
2024-09-27 18:39:38 -0300

    CARTEC

---
+++ js/code.js
@@ -0,0 +1,9 @@
+// Função para rolar para a seção de solução
+function scrollToSection() {
+    document.getElementById('solution').scrollIntoView({ behavior: 'smooth' });
+}
+
+// Função para o botão de contato
+function contact() {
+    alert('Obrigado pelo interesse! Em breve entraremos em contato.');
+}
